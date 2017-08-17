16ff99a0bd3cce5786a0932224978ac63ce0ed93
zarapeisker <zarapeisker@gmail.com>
2017-08-17 18:25:39 -0400

    allow cross-origin requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
-  # mount_devise_token_auth_for 'User', at: 'auth'
 
-resources :languages, only: [:index]  do
-  resources :teachers, only: [:index], controller: 'language_teachers'
-end
-resources :teachers, only: [:index]
+	match '*any' => 'application#options', :via => [:options]
+	  
+	  # mount_devise_token_auth_for 'User', at: 'auth'
 
-resources :app_countries, only: [:index]  do
-  resources :teachers, only: [:index], controller: 'country_teachers'
-end
+	resources :languages, only: [:index]  do
+	  resources :teachers, only: [:index], controller: 'language_teachers'
+	end
+	resources :teachers, only: [:index]
 
-resources :subjects, only: [:index]  do
-  resources :teachers, only: [:index], controller: 'subject_teachers'
-  resources :students, only: [:index], controller: 'subject_students'
-end
+	resources :app_countries, only: [:index]  do
+	  resources :teachers, only: [:index], controller: 'country_teachers'
+	end
+
+	resources :subjects, only: [:index]  do
+	  resources :teachers, only: [:index], controller: 'subject_teachers'
+	  resources :students, only: [:index], controller: 'subject_students'
+	end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
